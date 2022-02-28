84d4365c2beda043b3cdd453386fce8c477c670d
Tanjil Pranto <tanjil.hossain@metadesignsolutions.co.uk>
2022-02-28 13:48:04 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import MemoComp from "./Components/MemoComp";
 import SearchBox from "./Components/SearchBox";
 
 function App() {
+ 
+
   return (
     <div className="App">
       <Header />
       <SearchBox />
-      
-      <div id="imgContainer">
-        <div style={{display:'flex',flexDirection:'column'}}>
-        <text style={{color:"white",fontSize:"30px",alignItems:'center'}}>Hello world</text>
-        <text style={{placeItems:'flex-start'}}>Hello world</text>
-        </div>
-        
-      </div>
-      
+      <MemoComp/>
     </div>
   );
 }
