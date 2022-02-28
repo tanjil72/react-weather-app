4ff0034780c1143b4c32ded9c3b02bad8ae8d9ef
Tanjil Pranto <tanjil.hossain@metadesignsolutions.co.uk>
2022-02-28 14:20:25 +0600

    Memo comp doesn't needed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import MemoComp from "./Components/MemoComp";
 import SearchBox from "./Components/SearchBox";
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBox />
-      <MemoComp/>
+      
     </div>
   );
 }
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import MemoComp from "./MemoComp";
 
 export default function SearchBox() {
+  const [data, setData] = useState("");
+  const [address, setAddress] = useState("");
+  const [search, setSearch] = useState();
+
+  function handleclick() {
+    fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?key=XUGYEAM5JEZ4DKTFZ8W58T4NW`,
+      {}
+    )
+      .then((response) => response.json())
+      .then((Jsonresponse) => {
+        setData(Jsonresponse.currentConditions);
+        console.log(Jsonresponse);
+        setAddress(Jsonresponse.address);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    //console.log(search);
+  }
+
   return (
-    <div >
-        <input className="searchBox" type="text" placeholder="Search City" ></input>
-        <button id="searchButton">Search</button>
+    <div>
+      <input
+        className="searchBox"
+        type="text"
+        placeholder="Search City"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+      <button id="searchButton" onClick={handleclick}>
+        Search
+      </button>
+      <div id="imgContainer">
+        <text style={{ fontSize: "50px" }}>{address}</text>
+        <text style={{ fontSize: "40px", fontWeight: "bold" }}>
+          {data.temp} °F
+        </text>
+        <div>
+          <img
+            src="https://cdn-icons.flaticon.com/png/512/3222/premium/3222807.png?token=exp=1646024866~hmac=baafabc8c667338dc6806fea52889145"
+            width="70px"
+            height="70px"
+            alt="Weather forecast"
+          />
+        </div>
+        <text style={{ fontSize: "20px", marginBottom: "10px" }}>
+          {data.conditions}
+        </text>
+        <text style={{ fontSize: "20px" }}>{data.time}</text>
+      </div>
     </div>
-  )
+  );
 }
